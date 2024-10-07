1132893f00ba3e1b7b225a8c812a8d74389f905a
mohammad.safeea <mohammad.safeea@hilti.com>
2024-10-07 11:45:09 +0200

    add servo control over i2c

--- penta_i2c_actuators/launch/penta_i2c_actuators.launch.py
+++ penta_i2c_actuators/launch/penta_i2c_actuators.launch.py
@@ -1,22 +1,41 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    config = os.path.join(
-        get_package_share_directory("penta_description"),
-        "config",
-        "general_config.yaml",
+    # Declare the launch argument 'mode'
+    mode_arg = DeclareLaunchArgument(
+        'mode',
+        default_value='virtual',
+        description='Choose operation mode: real or virtual'
     )
 
-    the_node = Node(
-                    package='penta_i2c_actuators',
-                    executable='penta_i2c_actuators_node',
-                    output='screen',
-                    parameters=[config],
-                )
-    ld = LaunchDescription()
-    ld.add_action(the_node)
+    # Path to the config file
+    config = os.path.join(
+        get_package_share_directory('penta_description'),
+        'config',
+        'general_config.yaml'
+    )
+
+    # Node definition for I2C actuators
+    i2c_actuators_node = Node(
+        package='penta_i2c_actuators',
+        executable='penta_i2c_actuators_node',
+        output='screen',
+        parameters=[config],
+        # Pass 'mode' argument to the node
+        arguments=[LaunchConfiguration('mode')]
+    )
+
+    # Create a launch description and add the actions
+    ld = LaunchDescription([
+        mode_arg,            # Add the mode argument
+        i2c_actuators_node   # Add the node
+    ])
+
     return ld
--- penta_i2c_actuators/penta_i2c_actuators/penta_i2c_actuators_node.py
+++ penta_i2c_actuators/penta_i2c_actuators/penta_i2c_actuators_node.py
@@ -2,11 +2,12 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 import math
+import sys
 
 class PentaI2CActuators(Node):
-    def __init__(self):
+    def __init__(self, mode):
         super().__init__('penta_i2c_actuators')
-        self.get_logger().info('Starting I2C actuators control')
+        self.get_logger().info(f'Starting I2C actuators control in {mode} mode')
 
         # Declare and load parameters
         self.declare_params()
@@ -25,6 +26,18 @@ class PentaI2CActuators(Node):
         # Initialize the joint states position vector
         self.q = [0.0] * self.joints_count
         self.actuator_setpoint_degree = [0.0] * self.joints_count
+        self.servo_actuation_range_degree = [0.0] * self.joints_count
+        self.servo_min_pulse_width_microsec = [0.0] * self.joints_count
+        self.servo_max_pulse_width_microsec = [0.0] * self.joints_count
+        
+        # Set mode (real or virtual)
+        self.real_mode_flag = True if mode == 'real' else False
+        if self.real_mode_flag:
+            from adafruit_servokit import ServoKit
+            self.kit = ServoKit(channels=16)  # Use 16-channel board
+            for i in range(self.joints_count):
+                self.kit.servo[i].set_pulse_width_range(self.servo_min_pulse_width_microsec[i], self.servo_max_pulse_width_microsec[i]) 
+                self.kit.servo[i].actuation_range = self.servo_angle_range_degree[i]
 
         # Publisher to publish aggregated joint states
         self.joint_state_publisher_ = self.create_publisher(JointState, '/joint_states', 10)
@@ -49,7 +62,7 @@ class PentaI2CActuators(Node):
 
     def timer_callback(self):
         if not self.joints_pos_received:
-            self.get_logger().warn("joints angles not received yet, will not send commands on the i2c bus")
+            self.get_logger().warn("Joints angles not received yet, will not send commands on the I2C bus.")
             return
         # Publish aggregated joint states
         msg = JointState()
@@ -67,59 +80,83 @@ class PentaI2CActuators(Node):
         msg_setpoint.name = self.joints_states_names
         msg_setpoint.position = self.actuator_setpoint_degree
         self.setpoint_publisher_.publish(msg_setpoint)
+        # command the motors over i2c bus
+        if self.real_mode_flag:
+            # Actuate the motors
+            for i in range(self.joints_count):
+                self.kit.servo[i].angle = self.actuator_setpoint_degree[i]
     
     def declare_params(self):
         # Declare robot geometry parameters
-        self.declare_parameter('limbs_num', 200)  # Default value 5
+        self.declare_parameter('limbs_num', 5)  # Default value 5
         self.declare_parameter('joints_per_limb', 3)  # Default value 3
         self.declare_parameter('i2c_actuators_params.update_interval_millis', 100)  # Default 100 ms
         self.declare_parameter('i2c_actuators_params.actuator_angle_bias_at_joint_zero_degree', [0.0] * 15)  # Default bias
         self.declare_parameter('i2c_actuators_params.dir', [1.0] * 15)  # Default direction (1.0 for no inversion)
+        self.declare_parameter('servo_parameters.servo_angle_range_degree', [180.0] * 15) # angular range degree
+        self.declare_parameter('servo_parameters.servo_min_pulse_width_microsec', [500.0] * 15) # microseconds
+        self.declare_parameter('servo_parameters.servo_max_pulse_width_microsec', [2500.0] * 15) # microseconds
 
     def load_params(self):
         # Load parameters and handle errors
         self.limbs_num = self.get_parameter('limbs_num').get_parameter_value().integer_value
         self.joints_per_limb = self.get_parameter('joints_per_limb').get_parameter_value().integer_value
+        self.get_logger().info(f'Loaded limbs_num: {self.limbs_num}, joints_per_limb: {self.joints_per_limb}')
         self.joints_count = self.limbs_num * self.joints_per_limb
         self.update_interval_millis = self.get_parameter('i2c_actuators_params.update_interval_millis').get_parameter_value().integer_value
+        self.get_logger().info(f'Loaded update_interval_millis for I2C bus: {self.update_interval_millis} ms')
         self.initial_joints_bias_degree = self.get_parameter('i2c_actuators_params.actuator_angle_bias_at_joint_zero_degree').get_parameter_value().double_array_value
         self.dir = self.get_parameter('i2c_actuators_params.dir').get_parameter_value().double_array_value
+        self.servo_angle_range_degree = self.get_parameter('servo_parameters.servo_angle_range_degree').get_parameter_value().double_array_value
+        self.servo_min_pulse_width_microsec = self.get_parameter('servo_parameters.servo_min_pulse_width_microsec').get_parameter_value().double_array_value
+        self.servo_max_pulse_width_microsec = self.get_parameter('servo_parameters.servo_max_pulse_width_microsec').get_parameter_value().double_array_value
+
+        # Helper to format array for logging
+        def format_array_to_string(x_list):
+            return '[' + ', '.join(map(str, x_list)) + ']'
 
         if len(self.initial_joints_bias_degree) != self.joints_count:
-            self.get_logger().error('ERROR, actuator_angle_bias_at_joint_zero_degree parameter size mismatch!')
+            self.get_logger().error('ERROR: actuator_angle_bias_at_joint_zero_degree parameter size mismatch!')
+        else:
+            self.get_logger().info(f'Initial joints bias in degree is loaded: {format_array_to_string(self.initial_joints_bias_degree)}')
+
         if len(self.dir) != self.joints_count:
-            self.get_logger().error('ERROR, size mismatch in the array parameter describing the actuators rotation direction with respect to joints direction')
+            self.get_logger().error('ERROR: Direction array size mismatch with joints count!')
+        else:
+            self.get_logger().info(f'Direction array is loaded: {format_array_to_string(self.dir)}')
 
-        if self.limbs_num is not None:
-            self.get_logger().info(f'(limbs_num) parameter loaded and equal to: {self.limbs_num}')
-        else:
-            self.get_logger().error('ERROR, cannot load (limbs_num) parameter')
-        
-        if self.joints_per_limb is not None:
-            self.get_logger().info(f'(joints_per_limb) parameter loaded and equal to: {self.joints_per_limb}')
-        else:
-            self.get_logger().error('ERROR, cannot load (joints_per_limb) parameter')
+        if len(self.servo_angle_range_degree) != self.joints_count:
+            self.get_logger().error('ERROR: Servo angle range array size mismatch with joints count!')
+        else: 
+            self.get_logger().info(f'Servo joints angle range is loaded: {format_array_to_string(self.servo_angle_range_degree)}')
 
-        if self.update_interval_millis is not None:
-            self.get_logger().info(f'(update_interval_millis) parameter loaded and equal to: {self.update_interval_millis}')
-        else:
-            self.get_logger().error('ERROR, cannot load (update_interval_millis) parameter')
+        if len(self.servo_min_pulse_width_microsec) != self.joints_count:
+            self.get_logger().error('ERROR: Servo minimum pulse width array size mismatch!')
+        else: 
+            self.get_logger().info(f'Servos minimum pulse width is loaded: {format_array_to_string(self.servo_min_pulse_width_microsec)}')
 
-        if self.update_interval_millis is not None:
-            self.get_logger().info(f'(update_interval_millis) parameter loaded and equal to: {self.update_interval_millis}')
+        if len(self.servo_max_pulse_width_microsec) != self.joints_count:
+            self.get_logger().error('ERROR: Servo maximum pulse width array size mismatch!')
         else:
-            self.get_logger().error('ERROR, cannot load (update_interval_millis) parameter')
+            self.get_logger().info(f'Servos maximum pulse width is loaded: {format_array_to_string(self.servo_max_pulse_width_microsec)}')
 
     def on_joint_state_callback_limb(self, limb_index, joint_state):
         # Update the q vector with the received joint state positions for this limb
         index_start = limb_index * self.joints_per_limb
-        self.q[index_start:index_start+self.joints_per_limb] = joint_state.position[:self.joints_per_limb]  # Assuming joint state size matches joints_per_limb
+        self.q[index_start:index_start+self.joints_per_limb] = joint_state.position[:self.joints_per_limb]
         if not self.joints_pos_received:
             self.joints_pos_received = True
 
 def main(args=None):
     rclpy.init(args=args)
-    node = PentaI2CActuators()
+
+    # Retrieve the mode from arguments
+    mode = 'virtual'
+    if len(sys.argv) > 1:
+        mode = sys.argv[1]
+
+    # Create the node with the mode passed
+    node = PentaI2CActuators(mode=mode)
 
     # Keep the node alive to receive and process messages
     rclpy.spin(node)
