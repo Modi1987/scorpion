80a71dbe73e160d5e2b47501a74f9115954d649a
Safeea, Mohammad <mohammad.safeea@hilti.com>
2024-12-31 22:52:04 +0100

    wip

---
+++ penta_pod/launch/penta_sim_full_rviz.launch.py
@@ -0,0 +1,58 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
+import os
+
+def generate_launch_description():
+    # Include RVIZ launch file
+    rviz_penta_pod_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('penta_pod').find('penta_pod'), 'launch', 'penta_rviz.launch.py')
+        )
+    )
+
+    # Include the penta_pod launch file
+    penta_pod_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('penta_pod').find('penta_pod'), 'launch', 'penta_pod.launch.py')
+        )
+    )
+    
+    # Include the gait_generator launch file
+    gait_generator_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('gait_generator').find('gait_generator'), 'launch', 'gait_generator.launch.py')
+        )
+    )
+
+    # Include the joints_aggregator launch file
+    joints_aggregator_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('joints_aggregator').find('joints_aggregator'), 'launch', 'joints_aggregator.launch.py')
+        )
+    )
+
+    # Include the null space cmd publisher launch file
+    null_space_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('base_twerk').find('base_twerk'), 'launch', 'null_pose_publisher.launch.py')
+        )
+    )
+
+    # Include the twerk action server launch file
+    twerk_action_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('base_twerk').find('base_twerk'), 'launch', 'base_twerk_action_server.launch.py')
+        )
+    )
+
+    return LaunchDescription([
+        rviz_penta_pod_launch,
+        penta_pod_launch,
+        gait_generator_launch,
+        joints_aggregator_launch,
+        null_space_publisher,
+        twerk_action_server
+    ])
