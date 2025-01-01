7cd0fec0ef314a4ee2cd7e7fcc32f8a24c2db38b
Safeea, Mohammad <mohammad.safeea@hilti.com>
2025-01-01 13:22:36 +0100

    compile quatrenions utils

--- commons/src/commons/librarry.cpp
+++ commons/src/commons/librarry.cpp
@@ -1,4 +1,5 @@
 #include "commons/ros2_utils.hpp"
 #include "commons/transform_utils.hpp"
+#include "commons/quaternion_utils.hpp"
 
 namespace penta_pod::kin::commons {}; // namespace penta_pod::kin::commons
