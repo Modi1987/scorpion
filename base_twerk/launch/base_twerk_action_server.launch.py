6b628cc675d3721be5b33e0b19571b98861ad04a
Safeea, Mohammad <mohammad.safeea@hilti.com>
2025-05-10 23:53:02 +0200

    add namespace

--- base_twerk/launch/base_twerk_action_server.launch.py
+++ base_twerk/launch/base_twerk_action_server.launch.py
@@ -2,25 +2,41 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    """ Launch args """
+    name_space_arg = DeclareLaunchArgument(
+        "name_space",
+        default_value="",
+        description="Robot name space",
+    )
+    ld.add_action(name_space_arg)
+
+    """ Nodes """
     config = os.path.join(
         get_package_share_directory("penta_description"),
         "config",
         "general_config.yaml",
     )
-    ld = LaunchDescription()
     ld.add_action(
         Node(
             package="base_twerk",
             executable="base_twerk_action_server_node",
             output="screen",
+            namespace=LaunchConfiguration("name_space"),
             parameters=[config],
             remappings=[
-                ("cmd_null_setpoint", "/cmd_null_setpoint"),
-                ("get_current_null_pose", "/get_current_null_pose"),
-            ]
+                # services
+                ("cmd_null_setpoint", "base_twerk/cmd_null_setpoint"),
+                ("get_current_null_pose", "base_twerk/get_current_null_pose"),
+                # topics
+            ],
         )
     )
     return ld
--- penta_teleop/launch/teleop_joystick.launch.py
+++ penta_teleop/launch/teleop_joystick.launch.py
@@ -3,9 +3,22 @@ from launch_ros.actions import Node
 
 import os
 from ament_index_python.packages import get_package_share_directory
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    """ Launch args """
+    name_space_arg = DeclareLaunchArgument(
+        "name_space",
+        default_value="",
+        description="Robot name space",
+    )
+    ld.add_action(name_space_arg)
+
+    """ Nodes """
 
     joy_params = os.path.join(
         get_package_share_directory("penta_teleop"), "config", "teleop_joystick.yaml"
@@ -14,6 +27,7 @@ def generate_launch_description():
     joy_node = Node(
         package="joy",
         executable="joy_node",
+        namespace=LaunchConfiguration("name_space"),
         parameters=[joy_params],
     )
 
@@ -21,6 +35,7 @@ def generate_launch_description():
         package="teleop_twist_joy",
         executable="teleop_node",
         name="teleop_node",
+        namespace=LaunchConfiguration("name_space"),
         parameters=[joy_params],
         # remappings=[("/cmd_vel", "/input/cmd_vel_teleop_joy")],
     )
@@ -37,9 +52,21 @@ def generate_launch_description():
         package="penta_teleop",
         executable="joystick_extra_controls_node",
         name="joystick_extra_controls_node",
+        namespace=LaunchConfiguration("name_space"),
+        remappings=[
+            # services
+            ("cmd_null_setpoint", "base_twerk/cmd_null_setpoint"),
+            ("get_current_null_pose", "base_twerk/get_current_null_pose"),
+        ],
         parameters=[joystick_extra_controls_configs, general_config_params],
     )
 
-    return LaunchDescription(
-        [joy_node, teleop_twist_joy_node, joystick_extra_controls_node]
-    )
+    nodes = [
+        joy_node,
+        teleop_twist_joy_node,
+        joystick_extra_controls_node,
+    ]
+    for node in nodes:
+        ld.add_action(node)
+
+    return ld
