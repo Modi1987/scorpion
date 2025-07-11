15bd9023ccf78b9d9330977d9c5a5e17d4abc010
Safeea, Mohammad <mohammad.safeea@hilti.com>
2025-07-11 13:44:16 +0200

    ros2 to ros2 control, take into consideration offset and direction difference between real-robot and actuators

--- ros_to_ros2_control_command_bridge/launch/forward_joint_command_bridge.launch.py
+++ ros_to_ros2_control_command_bridge/launch/forward_joint_command_bridge.launch.py
@@ -2,7 +2,7 @@ import launch
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
-
+import os
 
 def generate_launch_description():
 
@@ -16,6 +16,12 @@ def generate_launch_description():
     )
     ld.add_action(name_space_arg)
 
+    config_file = os.path.join(
+        get_package_share_directory("penta_description"),
+        "config",
+        "general_config.yaml",
+    )
+    
     setpoint_to_frward_joint_position = Node(
         package="ros_to_ros2_control_command_bridge",
         executable="forward_joint_command_bridge_node",
@@ -27,6 +33,10 @@ def generate_launch_description():
             ),  # this is for the output topic
             ("joint_setpoints", "/joint_setpoints"),
         },  # this is for the input topic
+        parameters=[
+            {"name_space": launch.substitutions.LaunchConfiguration("name_space")},
+            config_file,
+        ],
     )
     ld.add_action(setpoint_to_frward_joint_position)
 
