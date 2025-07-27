ef02c464a8d86dbcb4a790110b256ee1016d1721
Safeea, Mohammad <mohammad.safeea@hilti.com>
2025-07-27 21:15:06 +0200

    rename cmd_vel_to_odom into twist_to_odom

--- cmd_vel_to_odom/src/cmd_vel_to_odom_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "cmd_vel_to_odom/cmd_vel_to_odom.hpp"
-
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  polypod::cmd_vel_to_odom::CmdVelToOdom().spin();
-  rclcpp::shutdown();
-  return 0;
-}
