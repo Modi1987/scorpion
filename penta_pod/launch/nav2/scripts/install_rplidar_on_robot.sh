4588c10b0b2184dbf91bd5532f2de8a3ded3b80d
Safeea, Mohammad <mohammad.safeea@hilti.com>
2025-08-02 14:03:46 +0200

    nav2 instllation scripts on the robot and on the PC

---
+++ penta_pod/launch/nav2/scripts/install_rplidar_on_robot.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIR"
+
+# Extract workspace root directory, by splitting on first src directory
+WORKSPACE_ROOT=$(awk -F'src' '{print $1}' <<< "$SCRIPT_DIR")
+
+ROS_DISTRO=humble
+
+# install vcs tools
+sudo apt install -y python3-vcstool
+sudo apt install -y python3-colcon-common-extensions
+
+# clone nav2 repos
+vcs import ${WORKSPACE_ROOT}/src < ./penta_nav_repos.yaml
+
+# Build the workspace
+cd $WORKSPACE_ROOT
+colcon build
