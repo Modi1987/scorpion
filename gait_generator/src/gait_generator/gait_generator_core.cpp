acde046cd8a4d7def398f57058061d140e0018aa
modesaf <ms@uc.pt>
2025-08-31 08:20:31 +0100

    give better naming

--- gait_generator/src/gait_generator/gait_generator_core.cpp
+++ gait_generator/src/gait_generator/gait_generator_core.cpp
@@ -204,13 +204,13 @@ void GaitGenerator::update_feet_positions(double delta_t_milli) {
           feet_pos_in_footprint_[foot_index].y -
           init_feet_pos_in_footprint_[foot_index].y;
     } else {
-      double delt_t = (2 * M_PI) / gait_parameters_.gait_radial_frequency;
-      double forward_step_length = -cmd_vel_.linear.x * delt_t * 0.5;
+      double step_interval_sec = (2 * M_PI) / gait_parameters_.gait_radial_frequency;
+      double forward_step_length = -cmd_vel_.linear.x * step_interval_sec * 0.5;
       feet_pos_in_footprint_[foot_index].x =
           init_feet_pos_in_footprint_[foot_index].x +
           foot_pos_xy_generator(current_phase_, phase_shift_vec_[i],
                                 final_displacement_[foot_index].x, forward_step_length, feet_num_);
-      double lateral_step_length = -cmd_vel_.linear.y * delt_t * 0.5;
+      double lateral_step_length = -cmd_vel_.linear.y * step_interval_sec * 0.5;
       feet_pos_in_footprint_[foot_index].y =
           init_feet_pos_in_footprint_[foot_index].y +
           foot_pos_xy_generator(current_phase_, phase_shift_vec_[i],
