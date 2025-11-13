9d61573976bca9a0cd900257d93deef59d890fb8
Safeea, Mohammad <mohammad.safeea@hilti.com>
2025-11-13 19:24:37 +0100

    revert majour changes and keep hiwonder vs hobby servo conditional launch

--- penta_pod/launch/realhardware_bringup.launch.py
+++ penta_pod/launch/realhardware_bringup.launch.py
@@ -3,11 +3,10 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import LaunchConfiguration, EqualsSubstitution
-from launch.conditions import IfCondition, UnlessCondition
+from launch.conditions import IfCondition
 import os
 
 def generate_launch_description():
-    ld = LaunchDescription()
     # Declare the 'mode' argument with a default value of 'real'
     mode_arg = DeclareLaunchArgument(
         'mode',
@@ -15,7 +14,6 @@ def generate_launch_description():
         description='Mode to run the actuators (real or sim)'
     )
     mode = LaunchConfiguration('mode')
-    ld.add_action(mode_arg)
 
     motors_interface_arg = DeclareLaunchArgument(
         'motors_interface',
@@ -23,7 +21,6 @@ def generate_launch_description():
         description='Type of motors to use (i2c or hiwonder)'
     )
     motors_interface = LaunchConfiguration('motors_interface')
-    ld.add_action(motors_interface_arg)
 
     # Include the penta_pod launch file
     penta_pod_launch = IncludeLaunchDescription(
@@ -31,7 +28,6 @@ def generate_launch_description():
             os.path.join(FindPackageShare('penta_pod').find('penta_pod'), 'launch', 'penta_pod.launch.py')
         )
     )
-    ld.add_action(penta_pod_launch)
     
     # Include the gait_generator launch file
     gait_generator_launch = IncludeLaunchDescription(
@@ -39,7 +35,6 @@ def generate_launch_description():
             os.path.join(FindPackageShare('gait_generator').find('gait_generator'), 'launch', 'gait_generator.launch.py')
         )
     )
-    ld.add_action(gait_generator_launch)
 
     # Include the joints_aggregator launch file
     joints_aggregator_launch = IncludeLaunchDescription(
@@ -47,17 +42,6 @@ def generate_launch_description():
             os.path.join(FindPackageShare('joints_aggregator').find('joints_aggregator'), 'launch', 'joints_aggregator.launch.py')
         )
     )
-    ld.add_action(joints_aggregator_launch)
-
-    # Include the penta_hiwonder_actuators launch file with the mode argument (when motors_interface == hiwonder)
-    penta_hiwonder_actuators_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(FindPackageShare('penta_hiwonder_actuators').find('penta_hiwonder_actuators'), 'launch', 'penta_hiwonder_actuators.launch.py')
-        ),
-        launch_arguments={'mode': mode}.items(),  # Pass the mode argument
-        condition=IfCondition(EqualsSubstitution(motors_interface, 'hiwonder'))
-    )
-    ld.add_action(penta_hiwonder_actuators_launch)
 
     # Include the penta_i2c_actuators launch file with the mode argument (when motors_interface != hiwonder)
     penta_i2c_actuators_launch = IncludeLaunchDescription(
@@ -67,7 +51,15 @@ def generate_launch_description():
         launch_arguments={'mode': mode}.items(),  # Pass the mode argument
         condition=IfCondition(EqualsSubstitution(motors_interface, 'i2c'))
     )
-    ld.add_action(penta_i2c_actuators_launch)
+
+    # Include the penta_hiwonder_actuators launch file with the mode argument (when motors_interface == hiwonder)
+    penta_hiwonder_actuators_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(FindPackageShare('penta_hiwonder_actuators').find('penta_hiwonder_actuators'), 'launch', 'penta_hiwonder_actuators.launch.py')
+        ),
+        launch_arguments={'mode': mode}.items(),  # Pass the mode argument
+        condition=IfCondition(EqualsSubstitution(motors_interface, 'hiwonder'))
+    )
 
     # Include the null space cmd publisher launch file
     null_space_publisher = IncludeLaunchDescription(
@@ -75,7 +67,6 @@ def generate_launch_description():
             os.path.join(FindPackageShare('base_twerk').find('base_twerk'), 'launch', 'null_pose_publisher.launch.py')
         )
     )
-    ld.add_action(null_space_publisher)
 
     # Include the twerk action server launch file
     twerk_action_server = IncludeLaunchDescription(
@@ -83,6 +74,15 @@ def generate_launch_description():
             os.path.join(FindPackageShare('base_twerk').find('base_twerk'), 'launch', 'base_twerk_action_server.launch.py')
         )
     )
-    ld.add_action(twerk_action_server)
 
-    return ld
+    return LaunchDescription([
+        mode_arg,
+        motors_interface_arg,
+        penta_pod_launch,
+        gait_generator_launch,
+        joints_aggregator_launch,
+        penta_i2c_actuators_launch,
+        penta_hiwonder_actuators_launch,
+        null_space_publisher,
+        twerk_action_server
+    ])
